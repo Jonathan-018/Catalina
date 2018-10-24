eed6957f3b0cc54bcb93e839fda4f3f930115478
Jonathan Ponce <38948129+Jonathan-018@users.noreply.github.com>
2018-10-24 07:43:27 -0500

    Add files via upload

---
+++ src/catalina/Catalina.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catalina;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import jdk.internal.util.xml.impl.Input;
+
+/**
+ *
+ * @author Poncejon9
+ */
+public class Catalina {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+        Scanner scan = new Scanner(new File("Input.txt"));
+        System.out.println();
+    }
+    
+}
